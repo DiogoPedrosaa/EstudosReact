73000ecafcae9655256ff8fc65a7adf666074268
Diogo de Melo Monte Pedrosa <diogo.pedrosa@financas.pref>
2024-10-15 09:08:34 -0300

    Pequenas Alterações

--- src/App.tsx
+++ src/App.tsx
@@ -10,18 +10,19 @@ import GaleriaImagens from './GaleriaImagens';
 const App: React.FC = () => {
   return (
     <div className="App">
-      <h1>Detalhes Cavalheiro da Luz</h1>
+      <h1>Detalhes Malenia blade of Miquella</h1>
 
       <Pesquisador
   placeholder="Digite o nome do Item"
   onPesquisar={(query) => console.log("Procurando por:", query)}
 />
 <div style={{ display: 'flex', gap: '20px', justifyContent: 'center' }}>
+
         <Card
-          nome="Cavaleiro da Luz"
+          nome="Malenia Blade of Miquella"
           classe="Guerreiro"
-          descricao="Um cavaleiro destemido que luta pela justiça. Possui grande força e habilidades com espadas."
-          imagemUrl=""  
+          descricao="Malenia, Blade of Miquella e Malenia, Goddess of Rot é um Chefe Semideus de duas fases em Elden Ring. Ela é irmã gêmea de Miquella e ganhou fama por sua lendária batalha contra Starscourge Radahn durante a Shattering, na qual ela liberou o poder da Podridão Escarlate e reduziu Caelid a ruínas"
+          imagemUrl="https://assetsio.gnwcdn.com/elden-ring-malenia-1_qpbTEkc.jpg?width=1200&height=1200&fit=crop&quality=100&format=png&enable=upscale&auto=webp"  
         />
 
   
@@ -29,26 +30,19 @@ const App: React.FC = () => {
 
       <TabelaHabilidades
         habilidades={[
-          { nome: "Explosão Sombria", descricao: "Libera uma explosão de energia negra." },
-          { nome: "Teletransporte", descricao: "Move-se rapidamente para outra posição." },
+          { nome: "Dança da Espada", descricao: "Malenia levanta a espada para a esquerda e dá 4 passos longos - após cada passo, ela ataca com uma série de ataques. Depois disso, ela salta em direção ao Tarnished com um ataque e executa um golpe final giratório, derrubando o alvo." },
+          { nome: "Agarrão pelo Pescoço", descricao: "Malenia levantará a mão em preparação para um ataque de agarramento e, em seguida, atacará em linha reta em direção ao jogador muito rapidamente. Se a garra acertar, ela jogará o jogador no ar e o empalará, causando 2.225 de dano de impulso básico, antes de jogar seu corpo no chão. Durante a Fase dois, este ataque pode ser imediatamente seguido por uma varredura voadora + ataque de impulso. Este ataque não pode ser bloqueado ou desviado." },
+          {nome: "Explosão Escarlate", descricao: "Apenas na fase dois. Malenia manterá seus movimentos originais da Fase um, no entanto, há uma chance de que ela faça uma continuação na maioria dos ataques, onde ela voa brevemente no ar e então bate com sua espada, causando uma enorme quantidade de dano e criando no chão um pequeno vórtice de podridão, que explode após um breve atraso e acumula podridão escarlate. A explosão também pode ocorrer a partir dos ataques violentos da Fase um, que ela pode acompanhar e resultar em uma Explosão Escarlate dupla."},
+          {nome: "", descricao: ""},
         ]}
       />
 
 <ListaItens
   itens={[
-    { nome: "Espada Mágica", tipo: "Arma", raridade: "Rara" },
-    { nome: "Poção de Vida", tipo: "Consumível", raridade: "Comum" },
-    { nome: "Elmo de Ferro", tipo: "Armadura", raridade: "Incomum" }
+    { nome: "Almas", tipo: "Consumível", raridade: "Garantido" },
+    { nome: "Grande runa de Malenia", tipo: "Consumível", raridade: "Garantido" },
+    { nome: "Elmo Escarlate", tipo: "Armadura", raridade: "Incomum" }
   ]}
-/>
-
-
-<DetalhesPersonagem
-  nome="Cavaleiro da Luz"
-  classe="Guerreiro"
-  nivel={35}
-  descricao="Um cavaleiro destemido que luta pela justiça."
-  habilidades={["Ataque Veloz", "Defesa de Aço", "Golpe de Luz"]}
 />
 
 
